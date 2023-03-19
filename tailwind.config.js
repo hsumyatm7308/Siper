a8ebbda839b1fcc7b28aae981de3b8b9f119a1c3
Hsu Myat Moe <hsumyatm7308@gmail.com>
2023-03-19 20:52:04 +0630

    19-3-23

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
@@ -10,9 +11,28 @@ module.exports = {
       'xl': '1280px',
       '2xl': '1536px',
     },
-    colors:{
-      'blue': '#14213d'
-    }
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white':'#ffffff',
+      'blue': {
+
+        100 : '#003049',
+        200 : '#1D3557',
+        300 : '#5F72B4',
+        400 : '#353BC4',
+        500 : '#D9D9D9',
+
+
+      },
+      'yellow': '#C9CB70',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
   },
   plugins: [],
 }
